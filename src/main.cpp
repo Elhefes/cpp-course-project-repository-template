ed0f1c9759a887847536295f7733ebaf2e1de9bc
Henri Palin <henri.palin@aalto.fi>
2023-11-01 19:15:10 +0200

    add sfml library to compilation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(){
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Dungeon Crawler");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+
+    }
+
 	std::cout << "Hello World" << std::endl;
 	return 0;
 }
