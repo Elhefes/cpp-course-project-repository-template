10ac4b15ea0500acb1c03112be3085e7b50e99ad
Daniil Parniukov <parniud1@aalto.fi>
2023-11-05 21:56:22 +0300

    Basic character class

--- src/Character/Character.h
+++ src/Character/Character.h
@@ -5,4 +5,23 @@
 #ifndef DUNGEONCRAWLER_CHARACTER_H
 #define DUNGEONCRAWLER_CHARACTER_H
 
+/**
+ * Base class for every "alive" entity in the dungeon
+ */
+class Character {
+ public:
+
+  /// @brief Checks if the creature is alive.
+  /// @return true if the creature is alive, false otherwise
+  [[nodiscard]] bool IsAlive() const { return health_ > 0; }
+
+  void TakeDamage(int damage) {}
+
+ protected:
+  /// @brief current health of the creature.
+  /// Note: could be made unsigned int, but I am afraid that at some point I will forget that it is an unsigned
+  /// int and write something like health = max(health - damage, 0) and give some creature 2**32 hp.
+  int health_;
+};
+
 #endif //DUNGEONCRAWLER_CHARACTER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main(){
-	std::cout << "Hello World" << std::endl;
+int main() {
+    std::cout << "Hello World" << std::endl;
     sf::Vector2u windowSize(800u, 600u);
-	sf::RenderWindow window(sf::VideoMode(windowSize), "Dungeon Crawler");
+    const uint WINDOW_WIDTH = 800u, WINDOW_HEIGHT = 600u;
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Dungeon Crawler");
     window.setFramerateLimit(60);
 
     while (window.isOpen()) {
 
     }
 
-	return 0;
+    return 0;
 }
