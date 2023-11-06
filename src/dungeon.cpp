c96054c3b7df2c1aab83e24d9b8f641efa1ec589
vmatias <matias.veikkola@aalto.fi>
2023-11-06 18:19:50 +0200

    tile system for dungeon

--- src/dungeon.cpp
+++ src/dungeon.cpp
@@ -1,4 +1,173 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
+const int tileSize = 10;
+
+sf::Color getRandomColor() {
+    return sf::Color(rand() % 256, rand() % 256, rand() % 256);
+}
+
+class Room {
+public:
+    int x, y, width, height;
+
+    std::vector<std::vector<sf::Color>> tileColors;
+
+    Room(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {
+        tileColors.resize(width, std::vector<sf::Color>(height));
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height; ++j) {
+                tileColors[i][j] = getRandomColor();
+            }
+        }
+    }
+};
+
+class Corridor {
+public:
+    int x, y, width, height;
+
+    std::vector<std::vector<sf::Color>> tileColors;
+
+    Corridor(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {
+        tileColors.resize(width, std::vector<sf::Color>(height));
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < height; ++j) {
+                tileColors[i][j] = getRandomColor();
+            }
+        }
+    }
+};
+
+void generateRooms(std::vector<Room>& rooms, int numRooms, int maxWidth, int maxHeight) {
+    while (rooms.size() < numRooms) {
+        int roomWidth = 3 + rand() % 9;  // Random width in terms of tiles
+        int roomHeight = 3 + rand() % 9;  // Random height in terms of tiles
+        int roomX = rand() % (maxWidth / tileSize - roomWidth);
+        int roomY = rand() % (maxHeight / tileSize - roomHeight);
+        Room newRoom(roomX, roomY, roomWidth, roomHeight);
+
+        // Check for overlaps with existing rooms
+        bool overlaps = false;
+        for (const Room& existingRoom : rooms) {
+            if (newRoom.x * tileSize < existingRoom.x  * tileSize + existingRoom.width * tileSize &&
+                newRoom.x * tileSize + newRoom.width * tileSize > existingRoom.x * tileSize &&
+                newRoom.y * tileSize < existingRoom.y * tileSize + existingRoom.height * tileSize &&
+                newRoom.y * tileSize + newRoom.height * tileSize > existingRoom.y * tileSize) {
+                overlaps = true;
+                break;
+            }
+        }
+
+        if (!overlaps) {
+            rooms.push_back(newRoom);
+        }
+    }
+}
+
+void generateCorridors(const std::vector<Room>& rooms, std::vector<Corridor>& corridors) {
+    for (size_t i = 0; i < rooms.size(); i++) {
+        for (size_t j = i + 1; j < rooms.size(); j++) {
+            // Calculate corridor dimensions in terms of tiles
+            int corridorX, corridorY, corridorWidth, corridorHeight;
+            bool vertical = false;
+
+            if (rooms[i].x + rooms[i].width < rooms[j].x) {
+                corridorX = rooms[i].x + rooms[i].width;
+                corridorY = rooms[i].y + rooms[i].height / 2 - 1;
+                corridorWidth = rooms[j].x - (rooms[i].x + rooms[i].width);
+                corridorHeight = 1;
+                vertical = true;
+            } else if (rooms[i].x > rooms[j].x + rooms[j].width) {
+                corridorX = rooms[j].x + rooms[j].width;
+                corridorY = rooms[i].y + rooms[i].height / 2 - 1;
+                corridorWidth = rooms[i].x - (rooms[j].x + rooms[j].width);
+                corridorHeight = 1;
+                vertical = true;
+            } else if (rooms[i].y + rooms[i].height < rooms[j].y) {
+                corridorX = rooms[i].x + rooms[i].width / 2 - 1;
+                corridorY = rooms[i].y + rooms[i].height;
+                corridorWidth = 1;
+                corridorHeight = rooms[j].y - (rooms[i].y + rooms[i].height);
+            } else {
+                corridorX = rooms[i].x + rooms[i].width / 2 - 1;
+                corridorY = rooms[j].y + rooms[j].height;
+                corridorWidth = 1;
+                corridorHeight = rooms[i].y - (rooms[j].y + rooms[j].height);
+            }
+
+            Corridor corridor(corridorX, corridorY, corridorWidth, corridorHeight);
+            corridors.push_back(corridor);
+        }
+    }
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(0)));
+
+    sf::Vector2u windowSize(800u, 600u);
+    sf::RenderWindow window(sf::VideoMode(windowSize), "Dungeon");
+    window.setFramerateLimit(60);
+
+    std::vector<Room> rooms;
+    std::vector<Corridor> corridors;
+    std::vector<Corridor> c;
+    int numRooms = 13;
+    int maxWidth = 800;
+    int maxHeight = 600;
+
+    generateRooms(rooms, numRooms, maxWidth, maxHeight);
+    generateCorridors(rooms, corridors);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        // Draw the corridors and rooms
+
+        // Rooms
+        for (const Room& room : rooms) {
+            for (int i = 0; i < room.width; ++i) {
+                for (int j = 0; j < room.height; ++j) {
+                    sf::RectangleShape tileShape(sf::Vector2f(tileSize, tileSize));
+                    tileShape.setPosition(sf::Vector2f((room.x + i) * tileSize, (room.y + j) * tileSize));
+                    tileShape.setFillColor(room.tileColors[i][j]);
+                    window.draw(tileShape);
+                }
+            }
+        }
+
+
+        // Corridors
+        for (const Corridor& corridor : corridors) {
+            for (int i = 0; i < corridor.width; ++i) {
+                for (int j = 0; j < corridor.height; ++j) {
+                    sf::RectangleShape tileShape(sf::Vector2f(tileSize, tileSize));
+                    tileShape.setPosition(sf::Vector2f((corridor.x + i) * tileSize, (corridor.y + j) * tileSize));
+                    tileShape.setFillColor(corridor.tileColors[i][j]);
+                    window.draw(tileShape);
+                }
+            }
+        }
+
+        window.display();
+    }
+
+    return 0;
+}
+
+
+
+/*
 
 #include <SFML/Graphics.hpp>
 #include <vector>
@@ -126,16 +295,16 @@ void generateCorridors(const std::vector<Room>& rooms, std::vector<Corridor>& co
 int main() {
     srand(static_cast<unsigned>(time(0)));
 
-    sf::Vector2u windowSize(800u, 600u);
+    sf::Vector2u windowSize(1920u, 1080u);
     sf::RenderWindow window(sf::VideoMode(windowSize), "Dungeon");
     window.setFramerateLimit(60);
 
     std::vector<Room> rooms;
     std::vector<Corridor> corridors;
     std::vector<Corridor> c;
-    int numRooms = 5;
-    float maxWidth = 800.0f;
-    float maxHeight = 600.0f;
+    int numRooms = 10;
+    float maxWidth = 1920.0f;
+    float maxHeight = 1080.0f;
 
     generateRooms(rooms, numRooms, maxWidth, maxHeight);
     generateCorridors(rooms, corridors, c);
@@ -180,7 +349,7 @@ int main() {
 
     return 0;
 }
-
+*/
 /*
 #include <iostream>
 #include <SFML/Graphics.hpp>
