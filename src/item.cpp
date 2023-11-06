9079edef61d2314a52b6e5ccecfcddbd6b444427
root <root@DESKTOP-L4L00GE>
2023-11-06 17:59:46 +0200

    add item and inventory classes

---
+++ src/item.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+class Item {
+public:
+    Item(std::string name, int quantity) : name(name), quantity(quantity) {}
+
+    void print() {
+        std::cout << "Item: " << name << " (Quantity: " << quantity << ")\n";
+    }
+
+private:
+    std::string name;
+    int quantity;
+};
+
+class Inventory {
+public:
+    void addItem(Item& item) {
+        items.push_back(item);
+    }
+
+    void printInventory() {
+        if (items.empty()) {
+            std::cout << "Inventory is empty.\n";
+        } else {
+            std::cout << "Inventory Contents:\n";
+            for (Item& item : items) {
+                item.print();
+            }
+        }
+    }
+
+private:
+    std::vector<Item> items;
+};
+
+
