f53664fea411ddd5e3cd73acaa919fefd37b0bb0
vmatias <matias.veikkola@aalto.fi>
2023-11-22 22:21:18 +0200

    added hpp files for dungeon and room

--- src/room.cpp
+++ src/room.cpp
@@ -3,25 +3,17 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include "room.hpp"
 
-
-class Room {
-public:
-    int x, y, width, height;
-
-    std::vector<std::vector<sf::Color>> tileColors;
-
-    Room(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {
-        tileColors.resize(abs(width), std::vector<sf::Color>(abs(height)));
-        for (int i = 0; i < abs(width); ++i) {
-            for (int j = 0; j < abs(height); ++j) {
-
-                tileColors[i][j] = getRandomColor();
-            }
+Room::Room(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {
+    tileColors.resize(abs(width), std::vector<sf::Color>(abs(height)));
+    for (int i = 0; i < abs(width); ++i) {
+        for (int j = 0; j < abs(height); ++j) {
+            tileColors[i][j] = getRandomColor();
         }
     }
-private:
-    sf::Color getRandomColor() {
+}
+
+sf::Color Room::getRandomColor() {
     return sf::Color(rand() % 256, rand() % 256, rand() % 256);
 }
-};
