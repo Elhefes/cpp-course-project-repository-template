b4dd00feb188723c6432a3f5179da73e546742b8
vmatias <matias.veikkola@aalto.fi>
2023-12-07 10:57:49 +0200

    added random seed

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include <SFML/Graphics.hpp>
 #include "game.hpp"
 #include "Test/test.hpp"
+#include <ctime>
+#include <cstdlib>
 
 int main() {
+    srand(static_cast<unsigned int>(time(0)));
     runTests();
     Game game;
     game.run();
